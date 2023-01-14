6021340c0a71b38b368eb958997ca31ac6e446e2
Francesco Calò <fcaloisme@gmail.com>
2023-01-14 12:48:53 +0100

    Bringing in content

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,28 +21,30 @@ function shiftHeadingDown() {
 }
 
 // https://astro.build/config
-
-// https://astro.build/config
 export default defineConfig({
-  markdown: {
-    extendDefaultPlugins: true,
-    draft: true,
-    shikiConfig: {
-      // Choose from Shiki's built-in themes (or add your own)
-      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-      theme: 'nord',
-      // Add custom languages
-      // Note: Shiki has countless langs built-in, including .astro!
-      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
-      //langs: [],
-      // Enable word wrap to prevent horizontal scrolling
-      wrap: true,
+    markdown: {
+        extendDefaultPlugins: true,
+        draft: true,
+        shikiConfig: {
+            // Choose from Shiki's built-in themes (or add your own)
+            // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+            theme: 'nord',
+            // Add custom languages
+            // Note: Shiki has countless langs built-in, including .astro!
+            // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+            //langs: [],
+            // Enable word wrap to prevent horizontal scrolling
+            wrap: true,
+        }
+    },
+    integrations: [mdx({
+        extendDefaultPlugins: true,
+        draft: true,
+        remarkPlugins: [rehypeAccessibleEmojis, readingTime, readingMdxTime],
+        rehypePlugins: [shiftHeadingDown]
+    }), tailwind(), image()],
+    server: {
+        host: true,
+        port: 3002
     }
-  },
-  integrations: [mdx({
-    extendDefaultPlugins: true,
-    draft: true,
-    remarkPlugins: [rehypeAccessibleEmojis, readingTime, readingMdxTime],
-    rehypePlugins: [shiftHeadingDown]
-  }), tailwind(), image()]
 });
