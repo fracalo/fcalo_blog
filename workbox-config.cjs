12233bad50aeb6907db6b1a711144fa26771ab0c
Francesco Calò <fcaloisme@gmail.com>
2023-02-05 22:30:52 +0100

    Adding sw

---
+++ workbox-config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    globDirectory: "dist/",
+    globPatterns: [
+        "**/*.{js,html,css,avif,png,jpeg,webp,jpg,woff2,woff,wasm,ico,svg,txt,xml}",
+    ],
+    swDest: "dist/sw",
+    //swSrc: "dist",
+    //ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
+};
